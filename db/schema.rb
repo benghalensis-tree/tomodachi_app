305bae9e77490626480d042f86a0c370df2097de
genki utsumi <zabanya0770@gmail.com>
2023-05-27 19:06:33 +0900

    feedsテーブルにcontentカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_26_054252) do
+ActiveRecord::Schema.define(version: 2023_05_27_100511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2023_05_26_054252) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "content"
   end
 
 end
